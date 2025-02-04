77e38b81b6765b9e6bf43a07e3b1297adbbeacf2
zakyfathurr <zakyfathurrahman2802@gmail.com>
2025-02-04 17:57:10 +0700

    🚀 ETS Repo

---
+++ ETS-Web-Pemrograman/ETS/no3/main.js
@@ -0,0 +1,8 @@
+function onlyNumberKey(evt) {
+
+    // Only ASCII character in that range allowed
+    let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
